691bddd602a3d59ff51ab8dca2892743e058b1eb
mlmesa <mlmesa.dev@gmail.com>
2026-04-24 17:28:33 -0600

    feat: add sitemap, centralize configuration, and update SEO metadata and assets

---
+++ src/constants/config.ts
@@ -0,0 +1,13 @@
+export const SITE_CONFIG = {
+  name: 'FarmasiShop',
+  contact: {
+    email: 'contacto@farmasishop.com',
+    whatsapp: '+52 55 7547 4913',
+    whatsappRaw: '5575474913',
+  },
+  seo: {
+    keywords: 'Farmasi, belleza, maquillaje, cuidado de la piel, emprendimiento, productos veganos, cosméticos, oportunidad de negocio',
+    siteName: 'FarmasiShop',
+    twitterHandle: '@farmasishop'
+  }
+};
